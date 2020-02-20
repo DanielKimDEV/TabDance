681c3be2bd044227e023ae5c0e1069cf70deb58f
daniel.dev <daniel.dev@kakaoenterprise.com>
2020-02-20 15:20:43 +0900

    Initial Commit

---
+++ TabDance/TabDance.h
@@ -0,0 +1,19 @@
+//
+//  TabDance.h
+//  TabDance
+//
+//  Created by Daniel Kim on 2020/02/20.
+//  Copyright © 2020 Daniel Kim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TabDance.
+FOUNDATION_EXPORT double TabDanceVersionNumber;
+
+//! Project version string for TabDance.
+FOUNDATION_EXPORT const unsigned char TabDanceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TabDance/PublicHeader.h>
+
+
